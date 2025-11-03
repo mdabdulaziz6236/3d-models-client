55ad818ca05e4028ee5b9a673ff91be03c5b2153
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-11-03 10:07:06 +0600

    In the Name of Allah , Projects setUp complited and installed many pakage

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-5xl text-amber-300 text-center'>This is Home page.</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/UpdateModel/UpdateModel.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateModel = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UpdateModel;
