fb008793700b36e9c25126ac04afa2580c5563f5
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-11-03 11:08:30 +0600

    Added register page

---
+++ src/Pages/AllModels/AllModels.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AllModels = () => {
+  return (
+    <div>
+      <h1 className="pt-5 text-5xl text-center font-bold"> All Models</h1>
+      <p className=" text-center pt-3 font-semibold  mb-10 ">Explore 3d models.</p>
+    </div>
+    
+  );
+};
+
+export default AllModels;
--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,10 +1,26 @@
 import { createBrowserRouter } from "react-router";
-import MainLayOut from '../Layout/MainLayout'
+import MainLayOut from "../Layout/MainLayout";
+import AllModels from "../Pages/AllModels/AllModels";
+import Register from "../Pages/Auth/Register";
+import Login from "../Pages/Auth/Login";
 
 export const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<MainLayOut></MainLayOut>
-    }
-    
-])
+  {
+    path: "/",
+    element: <MainLayOut></MainLayOut>,
+    children: [
+      {
+        path: "all-models",
+        Component: AllModels,
+      },
+      {
+        path: "register",
+        Component: Register,
+      },
+      {
+        path: "login",
+        Component:Login,
+      },
+    ],
+  },
+]);
